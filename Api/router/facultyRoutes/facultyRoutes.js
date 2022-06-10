f2382295553243e30546b19cfb6c3b635e3e3c8c
amit <amit.kumar@greychaindesign.com>
2022-06-10 20:43:46 +0530

    Getall faculty list from get method

--- Api/router/facultyRoutes/facultyRoutes.js
+++ Api/router/facultyRoutes/facultyRoutes.js
@@ -1,48 +1,64 @@
-const express=require('express');
-const router=express.Router();
-const Student=require('../model/facultySchema');
-const mongoose=require('mongoose');
+const express = require('express');
+const router = express.Router();
+const Student = require('../model/facultySchema');
+const mongoose = require('mongoose');
 
-router.get('/',(req,res,next)=>{
+router.get('/', (req, res, next) => {
     res.status(200).json({
-        message:"faculty get request."
+        message: "faculty get request."
     })
 });
 
-router.get('/name',(req,res,next)=>{
+router.get('/name', (req, res, next) => {
     res.status(200).json({
-        mesage:"My name is Amit Sharma."
+        mesage: "My name is Amit Sharma."
     })
 });
 
-router.post('/',(req,res,next)=>{
+router.post('/', (req, res, next) => {
     // res.status(200).json({
     //     message:"Post request."
     // })
     // console.log('value',req.body);
 
-    const student=new Student({
-        _id:new mongoose.Types.ObjectId,
-        name:req.body.name,
-        email:req.body.email,
-        gender:req.body.gender,
-        phone:req.body.phone,
+    const student = new Student({
+        _id: new mongoose.Types.ObjectId,
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        phone: req.body.phone,
     });
 
-    student.save().then(result=>{
+    student.save().then(result => {
         console.log(result);
         res.status(200).json({
-            newStudent:result
+            newFaculty: result
         })
     })
-    .catch(err=>{
-        console.log(err);
-        res.status(500).json({
-            error:err
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
         })
-    })
 
 });
 
+router.get('/facultyList', (req, res, next) => {
+    Student.find()
+        .then(result => {
+            res.status(200).json({
+                facultyList: result
+            })
+        })
+        .catch(err => {
+            console.log('err', err);
+            res.status(500).json({
+                err: err
+            })
+        })
+});
 
-module.exports=router;
+
+
+module.exports = router;
